d378eea7ed6eeec97606bb4a05d7db0571485a8f
Xingnan.Liu <wwwcomy@gmail.com>
2018-08-21 22:36:31 +0800

    test json setter

--- src/main/java/com/example/demo/entity/LogicGroup.java
+++ src/main/java/com/example/demo/entity/LogicGroup.java
@@ -19,234 +19,243 @@ import javax.persistence.Table;
 
 import org.springframework.util.StringUtils;
 
+import com.fasterxml.jackson.annotation.JsonSetter;
+
 @Entity
 @Table(name = "logic_group")
 public class LogicGroup {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "GROUP_ID", unique = true)
-    private long groupId;
+	@Id
+	@GeneratedValue
+	@Column(name = "GROUP_ID", unique = true)
+	private long groupId;
 
-    @Column(name = "GROUP_NAME")
-    private String groupName;
+	@Column(name = "GROUP_NAME")
+	private String groupName;
 
-    @Column(name = "CREATER_ID")
-    private String createrId;
+	@Column(name = "CREATER_ID")
+	private String createrId;
 
-    @Column(name = "CREATER_NAME")
-    private String createrName;
+	@Column(name = "CREATER_NAME")
+	private String createrName;
 
-    @Column(name = "PARENT_ID")
-    private Long parentId;
+	@Column(name = "PARENT_ID")
+	private Long parentId;
 
-    @Column(name = "CREATE_DATE")
-    private Date createDate;
+	@Column(name = "CREATE_DATE")
+	private Date createDate;
 
-    @Column(name = "LAST_UPDATE_DATE")
-    private Date lastUpdateDate;
+	@Column(name = "LAST_UPDATE_DATE")
+	private Date lastUpdateDate;
 
-    @Column(name = "VISIBLE")
-    private String visible;
+	@Column(name = "VISIBLE")
+	private String visible;
 
-    @Column(name = "EXTERNAL")
-    private String external;
+	@Column(name = "EXTERNAL")
+	private String external;
 
-    @Column(name = "HAS_CHILD")
-    private String hasChild;
+	@Column(name = "HAS_CHILD")
+	private String hasChild;
 
-    @Column(name = "IS_BUNDLE")
-    private String isBundle;
+	@Column(name = "IS_BUNDLE")
+	private String isBundle;
 
-    @Column(name = "BRIEF_DESCRIPTION")
-    private String briefDescription;
+	@Column(name = "BRIEF_DESCRIPTION")
+	private String briefDescription;
 
-    @Column(name = "ACCESS_ABLE_LEVELS")
-    private String accessAbleLevels;
+	@Column(name = "ACCESS_ABLE_LEVELS")
+	private String accessAbleLevels;
 
-    @Column(name = "CUT_POINT_LEVEL")
-    private String cutPointLevel;
+	@Column(name = "CUT_POINT_LEVEL")
+	private String cutPointLevel;
 
-    @Column(name = "GROUP_FILTER_NAME")
-    private String groupFilterName;
+	@Column(name = "GROUP_FILTER_NAME")
+	private String groupFilterName;
 
-    @Column(name = "DISPLAY_GROUP_NAME")
-    private String displayGroupName;
+	@Column(name = "DISPLAY_GROUP_NAME")
+	private String displayGroupName;
 
-    @Column(name = "TEMPLATETYPE")
-    private String templateType;
+	@Column(name = "TEMPLATETYPE")
+	private String templateType;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @CollectionTable(name = "GROUP_PARAMS", joinColumns = @JoinColumn(name = "GROUP_ID"))
-    @MapKeyColumn(name = "PARAM_KEY")
-    @Column(name = "PARAM_VALUE")
-    private Map<String, String> groupParams = new HashMap<String, String>();
+	@ElementCollection(fetch = FetchType.EAGER)
+	@CollectionTable(name = "GROUP_PARAMS", joinColumns = @JoinColumn(name = "GROUP_ID"))
+	@MapKeyColumn(name = "PARAM_KEY")
+	@Column(name = "PARAM_VALUE")
+	private Map<String, String> groupParams = new HashMap<String, String>();
 
-    public long getGroupId() {
-        return groupId;
-    }
+	public long getGroupId() {
+		return groupId;
+	}
 
-    public void setGroupId(long groupId) {
-        this.groupId = groupId;
-    }
+	public void setGroupId(long groupId) {
+		this.groupId = groupId;
+	}
 
-    public String getGroupName() {
-        return groupName;
-    }
+	public String getGroupName() {
+		return groupName;
+	}
 
-    public void setGroupName(String groupName) {
-        this.groupName = groupName;
-    }
+	public void setGroupName(String groupName) {
+		System.out.println("Check groupName:" + groupName);
+		this.groupName = groupName;
+	}
 
-    public String getCreaterId() {
-        return createrId;
-    }
+	@JsonSetter(value = "groupName")
+	public void setGroupNameOnly(String groupName) {
+		System.out.println("setGroupNameOnly" + groupName);
+		this.groupName = groupName;
+	}
 
-    public void setCreaterId(String createrId) {
-        this.createrId = createrId;
-    }
+	public String getCreaterId() {
+		return createrId;
+	}
 
-    public String getCreaterName() {
-        return createrName;
-    }
+	public void setCreaterId(String createrId) {
+		this.createrId = createrId;
+	}
 
-    public void setCreaterName(String createrName) {
-        this.createrName = createrName;
-    }
+	public String getCreaterName() {
+		return createrName;
+	}
 
-    public Long getParentId() {
-        return parentId;
-    }
+	public void setCreaterName(String createrName) {
+		this.createrName = createrName;
+	}
 
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
+	public Long getParentId() {
+		return parentId;
+	}
 
-    public Date getCreateDate() {
-        return createDate;
-    }
+	public void setParentId(Long parentId) {
+		this.parentId = parentId;
+	}
 
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
+	public Date getCreateDate() {
+		return createDate;
+	}
 
-    public Date getLastUpdateDate() {
-        return lastUpdateDate;
-    }
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
 
-    public void setLastUpdateDate(Date lastUpdateDate) {
-        this.lastUpdateDate = lastUpdateDate;
-    }
+	public Date getLastUpdateDate() {
+		return lastUpdateDate;
+	}
 
-    public String getVisible() {
-        return visible;
-    }
+	public void setLastUpdateDate(Date lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
 
-    public void setVisible(String visible) {
-        this.visible = visible;
-    }
+	public String getVisible() {
+		return visible;
+	}
 
-    public String getExternal() {
-        return external;
-    }
+	public void setVisible(String visible) {
+		this.visible = visible;
+	}
 
-    public void setExternal(String external) {
-        this.external = external;
-    }
+	public String getExternal() {
+		return external;
+	}
 
-    public String getHasChild() {
-        return hasChild;
-    }
+	public void setExternal(String external) {
+		this.external = external;
+	}
 
-    public void setHasChild(String hasChild) {
-        this.hasChild = hasChild;
-    }
+	public String getHasChild() {
+		return hasChild;
+	}
 
-    public String getIsBundle() {
-        return isBundle;
-    }
+	public void setHasChild(String hasChild) {
+		this.hasChild = hasChild;
+	}
 
-    public void setIsBundle(String isBundle) {
-        this.isBundle = isBundle;
-    }
+	public String getIsBundle() {
+		return isBundle;
+	}
 
-    public String getBriefDescription() {
-        return briefDescription;
-    }
+	public void setIsBundle(String isBundle) {
+		this.isBundle = isBundle;
+	}
 
-    public void setBriefDescription(String briefDescription) {
-        this.briefDescription = briefDescription;
-    }
+	public String getBriefDescription() {
+		return briefDescription;
+	}
 
-    public String getAccessAbleLevels() {
-        return accessAbleLevels;
-    }
+	public void setBriefDescription(String briefDescription) {
+		this.briefDescription = briefDescription;
+	}
 
-    public void setAccessAbleLevels(String accessAbleLevels) {
-        this.accessAbleLevels = accessAbleLevels;
-    }
+	public String getAccessAbleLevels() {
+		return accessAbleLevels;
+	}
 
-    public String getCutPointLevel() {
-        return cutPointLevel;
-    }
+	public void setAccessAbleLevels(String accessAbleLevels) {
+		this.accessAbleLevels = accessAbleLevels;
+	}
 
-    public void setCutPointLevel(String cutPointLevel) {
-        this.cutPointLevel = cutPointLevel;
-    }
+	public String getCutPointLevel() {
+		return cutPointLevel;
+	}
 
-    public String getGroupFilterName() {
-        return groupFilterName;
-    }
+	public void setCutPointLevel(String cutPointLevel) {
+		this.cutPointLevel = cutPointLevel;
+	}
 
-    public void setGroupFilterName(String groupFilterName) {
-        this.groupFilterName = groupFilterName;
-    }
+	public String getGroupFilterName() {
+		return groupFilterName;
+	}
 
-    public String getDisplayGroupName() {
-        return displayGroupName;
-    }
+	public void setGroupFilterName(String groupFilterName) {
+		this.groupFilterName = groupFilterName;
+	}
 
-    public void setDisplayGroupName(String displayGroupName) {
-        this.displayGroupName = displayGroupName;
-    }
+	public String getDisplayGroupName() {
+		return displayGroupName;
+	}
 
-    public String getTemplateType() {
-        return templateType;
-    }
+	public void setDisplayGroupName(String displayGroupName) {
+		this.displayGroupName = displayGroupName;
+	}
 
-    public void setTemplateType(String templateType) {
-        this.templateType = templateType;
-    }
+	public String getTemplateType() {
+		return templateType;
+	}
 
-    public Map<String, String> getGroupParams() {
-        return groupParams;
-    }
+	public void setTemplateType(String templateType) {
+		this.templateType = templateType;
+	}
 
-    public void setGroupParams(Map<String, String> groupParams) {
-        this.groupParams = groupParams;
-    }
+	public Map<String, String> getGroupParams() {
+		return groupParams;
+	}
 
-    public void update(LogicGroup logicGroup) {
-        this.displayGroupName = logicGroup.displayGroupName;
-        this.groupName = logicGroup.groupName;
-        updateParams(logicGroup.getGroupParams());
-    }
+	public void setGroupParams(Map<String, String> groupParams) {
+		this.groupParams = groupParams;
+	}
 
-    private void updateParams(Map<String, String> groupParams2) {
-        if (groupParams2 == null) {
-            return;
-        } else {
-            Set<Map.Entry<String, String>> entrySet = groupParams2.entrySet();
-            Iterator<Map.Entry<String, String>> i = entrySet.iterator();
-            while (i.hasNext()) {
-                Map.Entry<String, String> entry = i.next();
-                if (StringUtils.isEmpty(entry.getValue())) {
-                    groupParams.remove(entry.getKey());
-                } else {
-                    groupParams.put(entry.getKey(), entry.getValue());
-                }
-            }
-        }
+	public void update(LogicGroup logicGroup) {
+		this.displayGroupName = logicGroup.displayGroupName;
+		this.groupName = logicGroup.groupName;
+		updateParams(logicGroup.getGroupParams());
+	}
 
-    }
+	private void updateParams(Map<String, String> groupParams2) {
+		if (groupParams2 == null) {
+			return;
+		} else {
+			Set<Map.Entry<String, String>> entrySet = groupParams2.entrySet();
+			Iterator<Map.Entry<String, String>> i = entrySet.iterator();
+			while (i.hasNext()) {
+				Map.Entry<String, String> entry = i.next();
+				if (StringUtils.isEmpty(entry.getValue())) {
+					groupParams.remove(entry.getKey());
+				} else {
+					groupParams.put(entry.getKey(), entry.getValue());
+				}
+			}
+		}
+
+	}
 }
