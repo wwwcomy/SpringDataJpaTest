663e3328a60473b94a32f52bc10706842c1e40eb
Xingnan.Liu <wwwcomy@gmail.com>
2018-09-07 15:55:59 +0800

    Add rest template using apache http client

--- src/main/java/com/example/demo/Main1.java
+++ src/main/java/com/example/demo/Main1.java
@@ -1,5 +1,16 @@
 package com.example.demo;
 
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +20,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,36 +30,62 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Configuration
 @EnableAutoConfiguration
 @EnableJpaAuditing
-@ComponentScan(basePackages = "com.example.demo",
-    excludeFilters = {@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DemoApplication.class)})
+@ComponentScan(basePackages = "com.example.demo", excludeFilters = {
+		@ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = DemoApplication.class) })
 public class Main1 {
-    static Logger logger = LoggerFactory.getLogger(Main1.class);
+	static Logger logger = LoggerFactory.getLogger(Main1.class);
 
-    @Bean
-    public RestTemplate restOperations() {
-        RestTemplate rest = new RestTemplate();
-        // this is crucial!
-        rest.getMessageConverters().add(0, mappingJacksonHttpMessageConverter());
-        return rest;
-    }
+	@Bean
+	public RestTemplate restOperations() {
+		RestTemplate rest = new RestTemplate();
+		// this is crucial!
+		rest.getMessageConverters().add(0, mappingJacksonHttpMessageConverter());
+		return rest;
+	}
 
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter() {
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setObjectMapper(myObjectMapper());
-        return converter;
-    }
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJacksonHttpMessageConverter() {
+		MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+		converter.setObjectMapper(myObjectMapper());
+		return converter;
+	}
 
-    @Bean
-    public ObjectMapper myObjectMapper() {
-        ObjectMapper om = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        return om;
-    }
+	@Bean
+	public ObjectMapper myObjectMapper() {
+		ObjectMapper om = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		return om;
+	}
 
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(Main1.class);
-        app.setWebEnvironment(false);
-        app.run(args);
-    }
+	// @Bean
+	/**
+	 * This method is using Apache HttpClient as http implementation
+	 * 
+	 * @return
+	 * @throws KeyStoreException
+	 * @throws NoSuchAlgorithmException
+	 * @throws KeyManagementException
+	 */
+	public RestTemplate restTemplate() throws KeyStoreException, NoSuchAlgorithmException, KeyManagementException {
+		TrustStrategy acceptingTrustStrategy = (X509Certificate[] arg0, String arg1) -> true;
+
+		SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy)
+				.build();
+
+		SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+
+		CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+
+		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+
+		requestFactory.setHttpClient(httpClient);
+		RestTemplate restTemplate = new RestTemplate(requestFactory);
+		return restTemplate;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(Main1.class);
+		app.setWebEnvironment(false);
+		app.run(args);
+	}
 
 }
