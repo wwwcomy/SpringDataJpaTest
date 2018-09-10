accedf57d6d13952356e8d4d0163903c4156a6df
Xingnan.Liu <wwwcomy@gmail.com>
2018-09-10 10:54:20 +0800

    Add configuration properties note

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,34 +20,41 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+/**
+ * There's an annotation called @ConfigurationProperties to populate some config
+ * class properties, to replace @Value if there're lots of properties
+ * 
+ * @author xingnliu
+ *
+ */
 @SpringBootApplication
 @EnableSwagger2
 public class DemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
-    @Bean
-    public Docket createRestApi() {
-        ParameterBuilder tokenPar = new ParameterBuilder();
-        List<Parameter> pars = new ArrayList<Parameter>();
-        tokenPar.name("token").description("Token").modelRef(new ModelRef("string")).parameterType("header")
-            .required(false).build();
-        pars.add(tokenPar.build());
-        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
-            .apis(RequestHandlerSelectors.basePackage("com.example.demo.controller")).paths(PathSelectors.any()).build()
-            .globalOperationParameters(pars);
-    }
+	@Bean
+	public Docket createRestApi() {
+		ParameterBuilder tokenPar = new ParameterBuilder();
+		List<Parameter> pars = new ArrayList<Parameter>();
+		tokenPar.name("token").description("Token").modelRef(new ModelRef("string")).parameterType("header")
+				.required(false).build();
+		pars.add(tokenPar.build());
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select()
+				.apis(RequestHandlerSelectors.basePackage("com.example.demo.controller")).paths(PathSelectors.any())
+				.build().globalOperationParameters(pars);
+	}
 
-    private ApiInfo apiInfo() {
-        Contact contact = new Contact("wwwcomy", "", "wwwcomy@gmail.com");
-        return new ApiInfoBuilder().title("Task Manager").description("Task management service").contact(contact)
-            .version("1.0").build();
-    }
+	private ApiInfo apiInfo() {
+		Contact contact = new Contact("wwwcomy", "", "wwwcomy@gmail.com");
+		return new ApiInfoBuilder().title("Task Manager").description("Task management service").contact(contact)
+				.version("1.0").build();
+	}
 }
